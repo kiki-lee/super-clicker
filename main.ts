1b0c5d6e17da2a59dc6c79d5bc40f3bcd190fd2d
Kiki Prottsman <kiki-lee@users.noreply.github.com>
2021-05-06 10:21:08 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    info.changeScoreBy(1)
+    mySprite.setImage(assets.image`A static Down`)
+    projectile = sprites.createProjectileFromSprite(assets.image`Big Star`, mySprite, randint(-100, 100), randint(-50, -100))
+})
+info.onCountdownEnd(function () {
+    game.over(true, effects.confetti)
+})
+controller.A.onEvent(ControllerButtonEvent.Released, function () {
+    mySprite.setImage(assets.image`A static`)
+})
+let projectile: Sprite = null
+let mySprite: Sprite = null
+scene.setBackgroundImage(assets.image`TheStage`)
+game.splash("Press (A) to play!")
+info.startCountdown(10)
+mySprite = sprites.create(assets.image`A static`, SpriteKind.Player)
